623787e635cdc073d1f5db86482fe692d7f8c0fa
Raegan Lucero <raegano@tamu.edu>
2015-02-26 18:17:25 -0600

    Added rotation functionality
    Did some math stuff to make the line move.

--- project1/js/main.js
+++ project1/js/main.js
@@ -1,12 +1,17 @@
 var CANVAS_HEIGHT = window.innerHeight;
 var CANVAS_WIDTH = window.innerWidth * 0.75;
 
+var x1 = 200;
+var y1 = 250;
+var Th1 = 90;
 
 /**
  * setup code
 */   
 function setup() {
     createCanvas(CANVAS_WIDTH,CANVAS_HEIGHT);
+	angleMode(DEGREES);
+	noLoop();
 }
 
 
@@ -14,8 +19,15 @@ function setup() {
 * draw code (refreshes)
 */
 function draw() {
-    ellipse(200, 400, 80, 150);
-    ellipse(200,285,80,100);
-    ellipse(200,200,80,75);
+	background(204);
+    line(200, 400, x1, y1);
+	}
 
+function jointOneCW(){
+	//x+=10;
+	Th1-=1;
+	x1=200-150*cos(Th1);
+	y1=400+150*sin(Th1);
+	
+	redraw();
 }
